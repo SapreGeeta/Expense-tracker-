a444e10ea27885e72d5e45603c3fcc58752561bd
SapreGeeta <gitasapre@gmail.com>
2024-07-25 08:55:40 +0530

    mongodb is connected & server is running on port successfully

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { postSignup,postLogin } from "./controllers/user.js";
+import{postTransaction,getTransactions,deleteTransaction} from "./controllers/transation.js"
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//connect to MOngoDB
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URL)
+
+    if (conn) {
+        console.log(`MOngoDB connected Successfully...✅`);
+    }
+};
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: `welcome to expense API`
+    })
+})
+
+app.post("/signup",postSignup )
+
+app.post("/login",postLogin)
+
+app.post("/transaction",postTransaction)
+
+app.get("/transactions",getTransactions)
+
+
+app.delete("/transaction/:id", deleteTransaction)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
+
